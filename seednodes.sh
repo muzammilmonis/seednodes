10448324c27d8e8976a608f1e0a97d302c53361c
muzammilmonis <82541951+muzammilmonis@users.noreply.github.com>
2025-09-16 21:03:12 +0500

    Update seednodes.sh

--- seednodes.sh
+++ seednodes.sh
@@ -126,6 +126,31 @@ MYSQL_SCRIPT
 
     # Remove old nginx default
     rm /etc/nginx/sites-enabled/default
+
+    # Setup Pterodactyl Services
+    cat > /etc/systemd/system/pteroq.service <<EOL
+# Pterodactyl Queue Worker File
+# ----------------------------------
+
+[Unit]
+Description=Pterodactyl Queue Worker
+After=redis-server.service
+
+[Service]
+# On some systems the user and group might be different.
+# Some systems use `apache` or `nginx` as the user and group.
+User=www-data
+Group=www-data
+Restart=always
+ExecStart=/usr/bin/php /var/www/pterodactyl/artisan queue:work --queue=high,standard,low --sleep=3 --tries=3
+StartLimitInterval=180
+StartLimitBurst=30
+RestartSec=5s
+
+[Install]
+WantedBy=multi-user.target
+EOL
+    
     # Setup nginx
     cat > /etc/nginx/sites-available/pterodactyl.conf <<EOL
 server {
@@ -189,6 +214,9 @@ server {
     }
 }
 EOL
+
+    sudo systemctl enable --now redis-server
+    sudo systemctl enable --now pteroq.service
 
     ln -s /etc/nginx/sites-available/pterodactyl.conf /etc/nginx/sites-enabled/pterodactyl.conf
     nginx -t && systemctl restart nginx
